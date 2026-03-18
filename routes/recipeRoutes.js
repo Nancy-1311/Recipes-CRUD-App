d705a2cfc965a425d6c66d92d9f124725a6e21ed
Nancy-1311 <nancykhundia44@gmail.com>
2026-03-18 22:55:01 +0530

    Update recipeRoutes.js

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -2,14 +2,10 @@ const express = require("express");
 const router = express.Router();
 const recipeController = require("../controllers/recipeController");
 
-router.post("/recipes", recipeController.createRecipe);
-
-router.get("/recipes", recipeController.getAllRecipes);
-
-router.get("/recipes/:id", recipeController.getRecipeById);
-
-router.put("/recipes/:id", recipeController.updateRecipe);
-
-router.delete("/recipes/:id", recipeController.deleteRecipe);
+router.post("/", recipeController.createRecipe);
+router.get("/", recipeController.getAllRecipes);
+router.get("/:id", recipeController.getRecipeById);
+router.put("/:id", recipeController.updateRecipe);
+router.delete("/:id", recipeController.deleteRecipe);
 
 module.exports = router;
