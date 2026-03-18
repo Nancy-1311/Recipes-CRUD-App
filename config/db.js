acce11032fdc7d73bba68a59838206d067433e63
Nancy-1311 <nancykhundia44@gmail.com>
2026-03-18 23:26:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000
+    });
 
     console.log("MongoDB connected ✅");
   } catch (error) {
-    console.log(error);
+    console.log("DB connection error:", error);
+    process.exit(1);
   }
 };
 
